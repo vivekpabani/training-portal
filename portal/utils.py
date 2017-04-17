5e8545a466e4dc6182e739d2ba02417abbe93f92
Vivek Pabani <vpabani@hawk.iit.edu>
2017-04-16 19:51:54 -0500

    Rename path funtions and var

--- portal/utils.py
+++ portal/utils.py
@@ -3,20 +3,21 @@ import os
 from django.conf import settings
 
 
-def create_path_string(in_path):
+def create_abs_path(in_path):
     """
 
     """
 
-    in_path_string = os.path.join(*in_path.split('/'))
+    in_path = os.path.join(*in_path.split('/'))
 
-    abs_path_string = (os.sep).join([settings.BASE_DIR, settings.TOPICS_URL, in_path_string])
+    abs_path = (os.sep).join([settings.BASE_DIR, settings.TOPICS_URL, in_path])
 
-    return abs_path_string
+    return abs_path
 
-def is_valid_path(path_string):
+def is_valid_path(abs_path):
     """
 
     """
 
-    return os.path.exists(path_string)
+    return os.path.exists(abs_path_string)
+
